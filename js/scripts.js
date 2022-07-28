4694ed8f67253acce79962c9e75edb37830d9fe4
Kauã Muniz <kauamunizz@hotmail.com>
2022-07-28 14:36:49 -0300

    correcoes

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,33 @@
-const botoes = document.querySelectorAll('button');
+'strict mode'
 
-botoes.forEach(botao => {
-    botao.addEventListener('click', (event) => {
-        const elemento = event.currentTarget;
-        const resultado = document.getElementById('resultado');
-    
-        resultado.innerText += elemento.innerText;
-    })
+const scripts = (() => {
+    const resultado = document.getElementById('resultado');
 
-    document.getElementById('igual').addEventListener('click', () => {
-        resultado.innerText = eval(resultado.innerText);
-    })
+    function events() {
+        document.querySelectorAll('.btn-op').forEach(botao => {
+            botao.addEventListener('click', (event) => {
+                const elemento = event.currentTarget;
+            
+                resultado.innerText += elemento.innerText;
+            });
+        });
+        
+        document.getElementById('igual').addEventListener('click', () => {
+            resultado.innerText = eval(resultado.innerText);
+        });
+        
+        document.getElementById('del').addEventListener('click', () => {
+            resultado.innerText = resultado.innerText.slice(0, resultado.innerText.length-1);
+        });
+    }
+
+    function init() {
+        events();
+    }
     
-    document.getElementById('del').addEventListener('click', () => {
-        resultado.innerText =  resultado.innerText.slice(0, resultado.innerText.length-1);
-    })
-})
+    return {
+        init
+    }
+})();
+
+document.addEventListener('DOMContentLoaded', scripts.init);
